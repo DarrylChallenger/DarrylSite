878a2b44264306d92c975875e5931b3884c7526b
Darryl.Challenger <Darryl.Challenger@gmail.com>
2017-05-25 14:22:47 -0400

    Add test page on Private Menu, center text in grid

---
+++ DarrylSite/Controllers/TestsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DarrylSite.Controllers
+{
+    public class TestsController : Controller
+    {
+        // GET: Tests
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Text()
+        {
+            return View();
+        }
+    }
+}
