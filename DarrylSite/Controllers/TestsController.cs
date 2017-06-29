ffd0df17885bc1302a8b28543a33150baa144910
Darryl.Challenger <Darryl.Challenger@gmail.com>
2017-06-28 21:42:52 -0400

    Added Social Media button images, added jQuery, added random quotes.

--- DarrylSite/Controllers/TestsController.cs
+++ DarrylSite/Controllers/TestsController.cs
@@ -21,5 +21,9 @@ namespace DarrylSite.Controllers
         {
             return View();
         }
+        public ActionResult Social()
+        {
+            return View();
+        }
     }
 }
---
+++ DarrylSite/Scripts/RandomText.js
@@ -0,0 +1,27 @@
+﻿var quotes = [
+"A horse, a horse, my kingdom for a horse",
+"Et tu, Brute",
+"My mistress' eyes are nothing like the sun",
+"But at length the truth will out",
+"Cry havoc and let slip the dogs of war",
+"Jealousy is the green-eyed monster",
+"Once more into the breach",
+"To thine own self be true",
+"Such stuff as dreams are made of",
+"What the dickens",
+"What's done is done and cannot be undone",
+"Too much of a good thing",
+"Method to my madness",
+"Fight fire with fire",
+"All's well that ends well"];
+
+var s = document.getElementsByClassName("randomtext");
+var r = Math.random() * quotes.length;
+s[0].innerHTML = quotes[Math.floor(r)];
+
+/*
+$("document").ready(function() {
+    $(".randomtext2").text(quotes[Math.floor(r)]);
+});
+*/
+
